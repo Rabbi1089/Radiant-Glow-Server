e84dbcc8d048ceabb7b29f8ccd560fe672f7dc4e
Rabbi1089 <rabbi.ext@gmail.com>
2024-11-16 23:05:08 +0600

    create POST API for user service

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware 
-app.use(cors());
+app.use(cors({
+  
+    origin : ['http://localhost:5173'],
+    credentials : true
+  
+}));
 app.use(express.json());
 
 console.log(process.env.DB_Pass);
@@ -27,12 +32,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+ 
+
+    const serviceCollection = client.db("Radiant-Glow").collection("services");
+
+    app.post('/services' ,async (req, res ) => {
+      const service = req.body;
+      console.log(service);
+      const result = await serviceCollection.insertOne(service)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+ //   await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
