2397446216afa6c4183d09fe8e57f85631021849
Rabbi1089 <rabbi.ext@gmail.com>
2024-11-18 00:50:18 +0600

    add get api for  popular service and all services

--- index.js
+++ index.js
@@ -43,6 +43,17 @@ async function run() {
       res.send(result)
     })
 
+        // Get all jobs data from db
+        app.get("/services", async (req, res) => {
+          const result = await serviceCollection.find().toArray();
+          res.send(result);
+        });
+
+                // Get all jobs data from db
+          app.get("/trendServices", async (req, res) => {
+          const result = await serviceCollection.find().limit(6).toArray()
+          res.send(result);
+          });
 
     // Send a ping to confirm a successful connection
  //   await client.db("admin").command({ ping: 1 });
